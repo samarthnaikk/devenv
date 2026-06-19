3f1fafd2132551e6c132c19bf1bbc200927d5056
Samarth Naik <naikm.sam@gmail.com>
2026-06-20 00:36:26 +0530

    Add React website bootstrap

---
+++ interface/website/main.js
@@ -0,0 +1,5 @@
+import React from "https://esm.sh/react@18";
+import { createRoot } from "https://esm.sh/react-dom@18/client";
+import { App } from "./src/App.js";
+
+createRoot(document.getElementById("root")).render(React.createElement(App));
---
+++ interface/website/src/App.js
@@ -0,0 +1,9 @@
+import React from "https://esm.sh/react@18";
+
+export function App() {
+  return React.createElement(
+    "div",
+    { className: "loading-shell" },
+    "Booting Devenv web interface..."
+  );
+}
